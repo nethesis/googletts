e18a775ff7b21f29e3fe842f9a6b8cf36ee5a0d8
Stefano Fancello <stefano.fancello@nethesis.it>
2018-10-12 10:56:56 +0200

    Fix name in header

--- functions.inc.php
+++ functions.inc.php
@@ -4,20 +4,20 @@
 #    Copyright (C) 2018 Nethesis S.r.l.
 #    http://www.nethesis.it - support@nethesis.it
 #
-#    This file is part of TTS FreePBX module.
+#    This file is part of GoogleTTS FreePBX module.
 #
-#    TTS module is free software: you can redistribute it and/or modify
+#    GoogleTTS module is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or any 
 #    later version.
 #
-#    TTS module is distributed in the hope that it will be useful,
+#    GoogleTTS module is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with TTS module.  If not, see <http://www.gnu.org/licenses/>.
+#    along with GoogleTTS module.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 function googletts_get_options() {
